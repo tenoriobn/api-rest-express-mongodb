95eba1520347a895d19eda39055170143603ffc8
Bruno Tenório <tenoriobn@gmail.com>
2024-09-30 12:04:07 -0300

    evoluindo a api com controllers

---
+++ src/controllers/livroController.js
@@ -0,0 +1,56 @@
+import livro from "../models/Livro.js"
+
+class LivroController {
+
+  static async listarLivros (req, res) {
+    try {
+      const listaLivros = await livro.find({});
+      res.status(200).json(listaLivros);
+    } catch (error) {
+      res.status(500).json({ message: `${error.message} - falha na requisição!` });
+    }
+  }
+
+  static async listarLivroPorId (req, res) {
+    try {
+      const id = req.params.id;
+      const livroEncontrado = await livro.findById(id);
+
+      res.status(200).json(livroEncontrado);
+    } catch (error) {
+      res.status(500).json({ message: `${error.message} - falha na requisição do livro!` });
+    }
+  }
+
+  static async cadastrarLivro (req, res) {
+    try {
+      const novoLivro = await livro.create(req.body)
+      res.status(201).json({ message: "craido com sucesso", livro: novoLivro});
+    } catch (error) {
+      res.status(500).json({ message: `${error.message} - falha ao cadastrar livro!` })
+    }
+  }
+
+  static async atualizarLivro (req, res) {
+    try {
+      const id = req.params.id;
+      await livro.findByIdAndUpdate(id, req.body);
+      res.status(200).json({ message: "livro atualizado!" });
+    } catch (error) {
+      res.status(500).json({ message: `${error.message} - falha na atualização!` });
+    }
+  }
+
+  static async excluirLivro (req, res) {
+    try {
+      const id = req.params.id;
+      await livro.findByIdAndDelete(id);
+
+      res.status(200).json({ message: "livro excluído com sucesso!" });
+    } catch (error) {
+      res.status(500).json({ message: `${error.message} - falha na exclusão!` })
+    }
+  }
+};
+
+export default LivroController;
